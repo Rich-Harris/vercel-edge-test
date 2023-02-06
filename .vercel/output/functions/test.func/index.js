0568b3b1bce087ec36ea28ba616d5e280678413e
Rich Harris <hello@rich-harris.dev>
2023-02-06 14:41:51 -0500

    hmm

--- .vercel/output/functions/test.func/index.js
+++ .vercel/output/functions/test.func/index.js
@@ -1,6 +1,7 @@
 export default function handler(request) {
 	return Response.json({
 		VERCEL_REGION: process.env.VERCEL_REGION || 'undefined',
+		['x-vercel-edge-region']: request.headers.get('x-vercel-edge-region'),
 		['x-vercel-ip-country-region']: request.headers.get(
 			'x-vercel-ip-country-region'
 		)
